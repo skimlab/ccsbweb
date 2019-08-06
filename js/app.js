0bf48a90c595e3f953665418e005c5aadbf7dab3
Seungchan Kim <dolchan@gmail.com>
2019-08-06 10:19:11 -0500

    Adding flexslider fix for navigation arrows.
    Navigation arrows were too big.

--- js/app.js
+++ js/app.js
@@ -2,6 +2,11 @@
 $(window).load(function() {
     $('.flexslider').flexslider({
     	animation: "slide",
+    	start: function(slider){
+          $('body').removeClass('loading');
+        },
+        prevText: '<span class="flex-custom-nav-button--left"></i>',
+        nextText: '<span class="flex-custom-nav-button---right"></i>',
     	directionNav: true,
     	slideshowSpeed: 5000,
     	animationSpeed: 600,
